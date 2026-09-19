938ace5c722cf929cab7cbe571126d5097f05e8b
rootusershreyas <shreyasv@Technoidentity.com>
2026-09-19 19:49:23 +0530

    Prac: Chess Board and Grains problem - chessboard has 64squares, square 1 has 1grain , square 2 has 2 grains, till 2^(n-1)

--- Rust_practice/src/bin/ChessBoardGrains.rs
+++ Rust_practice/src/bin/ChessBoardGrains.rs
@@ -1,16 +1,21 @@
-fn square_grains(square: u32) -> u64 {
+fn grains_on_square(square: u32) -> u64 {
     2u64.pow(square - 1)
 }
 
 fn total_grains() -> u64 {
     let mut total = 0;
+
     for square in 1..=64 {
-        total += square_grains(square);
+        total += grains_on_square(square);
     }
+
     total
 }
 
 fn main() {
-    let _total = total_grains();
-    println!("Total grains on the chessboard: {}", _total);
+    let square = 10;
+
+    println!("Grains on square {}: {}", square, grains_on_square(square));
+
+    println!("Total grains on the chessboard: {}", total_grains());
 }
